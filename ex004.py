0940e34e8f2f4d712f9903b71e32b5c206114a2b
Marcelo Costa <49349361+OdZarref@users.noreply.github.com>
2019-05-28 10:12:54 -0300

    Create ex004.py

---
+++ ex004.py
@@ -0,0 +1,15 @@
+#ex004: Crie um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele.
+num = str(input('Digite algo: '))
+#Verá se é numérico
+print('É numérico? ', end='')
+print(num.isnumeric())
+#Verá se é alfabético
+print('É alfabético? ', end='')
+print(num.isalpha())
+#Verá se é alfanumérico
+print('É alfanumérico? ', end='')
+print(num.isalnum())
+#Verá se está totalmente em maiúsculo
+print('Está totalmente em maiúsculo? ', end='')
+print(num.isupper())
+input('Pressione qualquer tecla para sair.')
