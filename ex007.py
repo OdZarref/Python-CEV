d8ba43a69d2b5830e3c81de52b9651d1df88ae7b
Marcelo Costa <49349361+OdZarref@users.noreply.github.com>
2019-05-28 11:05:34 -0300

    Create ex007.py

---
+++ ex007.py
@@ -0,0 +1,6 @@
+#ex007: Crie um programa que leia duas notas de um aluno, calcule e mostre a sua média.
+print('CALCULE A SUA MÉDIA')
+nota1 = float(input('Qual a primeira nota? '))
+nota2 = float(input('Qual a segunda nota? '))
+print(f'A média é {(nota1 + nota2) / 2}')
+input('Pressione qualquer tecla para sair.')
