850b91da5198fecfda740f7f8688b1f77f3a3a55
Marcelo Costa <49349361+OdZarref@users.noreply.github.com>
2019-05-28 10:02:09 -0300

    Create ex002.py

---
+++ ex002.py
@@ -0,0 +1,6 @@
+#ex002: Crie um script que leia o dia, o mês e o ano de nascimento de uma pessoa e mostre uma mensagem com a data formatada.
+dia = str(input('Digite o seu dia de nascimento: '))
+mes = str(input('Digite o seu mês de nascimento: '))
+ano = str(input('Digite o seu ano de nascimento: '))
+print(f'Você nasceu em {dia}/{mes}/{ano}, certo?')
+input('Pressione qualquer tecla para sair.')
