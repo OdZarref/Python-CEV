90497daf1fcf3b739270f8ee529f39cccf3a4a0d
Marcelo Costa <49349361+OdZarref@users.noreply.github.com>
2019-05-28 10:09:45 -0300

    Create ex003.py

---
+++ ex003.py
@@ -0,0 +1,5 @@
+#ex003: Crie um script que leia dois números e tente mostrar a soma entre eles.
+num1 = int(input('Digite o primeiro valor: '))
+num2 = int(input('Digite o segundo valor: '))
+print(f'A soma entre eles é {num1 + num2}.')
+input('Pressione qualquer tecla para sair.')
