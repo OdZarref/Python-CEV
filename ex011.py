eff5d257c6e735770054d2cbb05e76028566e09a
Od <ludwigvonmarcelo@gmail.com>
2019-08-03 16:35:55 -0300

    adding exercices 11 and 12

---
+++ ex011.py
@@ -0,0 +1,6 @@
+#ex011: Faça um programa que leia a largura e altura de uma parede em metros, calcule a sua área e a quantidade de tintas necessárias para pintá-la, sabendo que cada litro de tinta, pinta uma área de 2m²
+altura = float(input('ALTURA: '))
+largura = float(input('LARGURA: '))
+area = altura * largura
+print(f'Será necessário {area / 2} litros de tinta.')
+input('pressione qualquer tecla para sair')
---
+++ ex012.py
@@ -0,0 +1,6 @@
+#ex012: Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+print('DIGITE UM PREÇO E VEJA-O COM DESCONTO DE 5%.')
+preco = float(input(': '))
+print('Com desconto de 5% o preço fica: {} R$.'.format(preco / 100 * 95))
+print('-=-' * 10)
+input('pressione qualquer tecla para sair')
