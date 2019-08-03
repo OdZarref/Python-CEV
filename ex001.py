7c32eb7fee968fca021e5903d278192843369ca6
Marcelo Costa <49349361+OdZarref@users.noreply.github.com>
2019-08-03 12:34:42 -0300

    Update ex001.py

--- ex001.py
+++ ex001.py
@@ -1,4 +1,4 @@
 #Crie um script Python que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas de acordo com o valor digitado.
 nome = str(input('Qual o seu nome? '))
 print(f'Seja bem-vindo, {nome}!')
-input('pressione qualquer tecla para sair')
+input('pressione qualquer tecla para sair.')
