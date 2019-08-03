680be5e658c687db44ecbd1e0c607b5c303023c3
Od <ludwigvonmarcelo@gmail.com>
2019-08-03 14:17:15 -0300

    Adicionando ex009.py e ex010.py

---
+++ ex010.py
@@ -0,0 +1,7 @@
+#ex010: Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar. Considere US$ 1,00 = R$ 3,27.'''
+print('CONVERSOR DE REAL PARA DOLAR\nDOLAR = 3.27')
+print('')
+reais = float(input('REAL: '))
+print('É possível comprar {} dólares.'.format(reais / 3.27))
+print('-=-' * 10)
+input('pressione qualquer tecla para sair.')
