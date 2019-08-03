5ee6983055576fe41a8c865293edcab5a7f26094
Marcelo Costa <49349361+OdZarref@users.noreply.github.com>
2019-08-03 13:30:38 -0300

    Update ex006.py

--- ex006.py
+++ ex006.py
@@ -1,4 +1,4 @@
 #ex006: Crie um programa que leia um número e mostre na tela o seu dobro, triplo e raiz quadrada.
 num = int(input('Digite um número: '))
 print(f'O seu dobro é {num * 2}, o seu triplo é {num * 3} e a sua raiz quadrada é {num ** (1/2)}')
-input('Pressione qualquer tecla para sair.')
+input('Pressione qualquer tecla para sair')
