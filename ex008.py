bae4f71ec8ce39f34ca8ff4d21f65a9aa70e40bb
Od <ludwigvonmarcelo@gmail.com>
2019-08-03 14:00:50 -0300

    Create ex008.py

---
+++ ex008.py
@@ -0,0 +1,4 @@
+#ex008: Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+distancia = float(input('Metros: '))
+print(f'A distância {distancia} em centímetros é {distancia * 100}. E em melímetros é {distancia * 1000}')
+input('pressione qualquer tecla para sair')
