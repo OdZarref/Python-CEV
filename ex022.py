2de9935828a58db9ecca47d14fcf7c8ecd8f8c91
Od Zarref <ludwigvonmarcelo@gmail.com>
2019-08-06 14:47:18 -0300

    Adding ex022.py

---
+++ ex022.py
@@ -0,0 +1,7 @@
+#ex022: Crie um programa que leia o nome completo de uma pessoa e mostre: O nome com todas as letras maiúsculas e minúsculas. Quantas letras ao todo(sem considerar espaços). Quantas letras tem o primeiro nome.
+nome = str(input('Qual é o seu nome? ')).strip()
+print(f'O seu nome completamente em minúsculo é: {nome.lower()}')
+print(f'O seu nome completamente em maiúsculo é: {nome.upper()}')
+print('O seu nome tem {} letras.'.format(len(nome) - nome.count(' ')))
+dividido = nome.split()
+print('O seu primeiro nome é {} e tem {} letras.'.format(dividido[0], len(dividido[0])))
