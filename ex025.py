ae460c3bbab9b0e12681d9aaac0c11b423915356
Od Zarref <ludwigvonmarcelo@gmail.com>
2019-08-06 17:46:04 -0300

    Adding ex025.py

--- ex025.py
+++ ex025.py
@@ -1,3 +1,5 @@
 #ex025: Crie um programa que leia o nome de uma pessoa e diga se ela tem ‘SILVA’ no nome.
 name = str(input('Qual o seu nome completo? ')).strip().upper()
 print('Você tem silva no nome? {}'.format('SILVA' in name))
+print('-=' * 10)
+input('pressione ENTER para sair')
