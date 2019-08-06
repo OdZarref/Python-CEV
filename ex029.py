931d3bb12e82160c3abc47836876113aaa2efb32
Od Zarref <ludwigvonmarcelo@gmail.com>
2019-08-06 20:02:01 -0300

    Adding ex029.py

---
+++ ex029.py
@@ -0,0 +1,8 @@
+#ex029: Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80 km/h, mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$ 7,00 por cada Km acima do limite.
+vel = int(input('Velocidade: '))
+if vel > 80:
+	print(f'Você foi multado por excesso de velocidade! O valor da multa é R${(vel - 80) * 7}.')
+else:
+	print('Tudo nos conformes. Tenha um boa viagem!')
+print('-=' * 10)
+input('pressione ENTER para sair')
