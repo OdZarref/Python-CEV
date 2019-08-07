8a9e40ff35d25a96ce823c27885f13a5fe9f7920
Od Zarref <ludwigvonmarcelo@gmail.com>
2019-08-06 21:12:26 -0300

    Adding ex033.py

--- ex033.py
+++ ex033.py
@@ -2,11 +2,15 @@
 num1 = int(input('Digite o primeiro número: '))
 num2 = int(input('Digite o segundo número: '))
 num3 = int(input('Digite o terceiro número: '))
-if num1 > num2 and num1 > num3:
-	print(f'O primeiro número ({num1}) é o maior.')
-elif num2 > num1 and num2 > num3:
-	print(f'O segundo número ({num2}) é o maior.')
+maior = menor = num1
+#maior
+if num2 > num1 and num2 > num3:
+	maior = num2
 elif num3 > num1 and num3 > num2:
-	print(f'O terceiro número ({num3}) é o maior.')
-print('-=' * 10)
-input('pressione ENTER para sair')
+	maior = num3
+#menor
+if num2 < num1 and num2 < num3:
+	menor = num2
+elif num3 < num1 and num3 < num2:
+	menor = num3
+print(f'O menor número é {menor} e o maior número é {maior}.')
