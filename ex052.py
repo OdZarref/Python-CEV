5d597e39b16276e698bfa9d5fd52b8895dc03d82
Od Zarref <ludwigvonmarcelo@gmail.com>
2019-08-11 19:27:22 -0300

    Resolving ex052.py

--- ex052.py
+++ ex052.py
@@ -1,1 +1,13 @@
 #Faça um programa que leia um número inteiro e diga se ele é ou não um número primo.
+cont = 0
+num = int(input('NÚMERO: '))
+print('-=' * 30)
+for c in range(1, num + 1):
+	if num % c == 0:
+		cont += 1
+if cont == 2:
+	print(f'Este número é primo. Pois foi divisível apenas por 2 números.')
+else:
+	print(f'Este número foi divisível {cont} vezes. Portanto não é primo.')
+print('-=' * 30)
+input('pressione ENTER para sair')
