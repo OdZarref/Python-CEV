38b7406b5dcab6ad4fa228bd866fd68438ad7ab1
Od Zarref <ludwigvonmarcelo@gmail.com>
2019-08-11 18:25:38 -0300

    Resolving ex049.py

--- ex049.py
+++ ex049.py
@@ -1,1 +1,6 @@
 #Refaça o DESAFIO 009, mostrando a tabuada de um número que o usuário escolher, que agora utilizando um laço for.
+tabuada = int(input('Tabuada: '))
+for c in range(1, 11):
+	print(f'{tabuada} x {c:2} = {tabuada * c}')
+print('-=' * 10)
+input('pressione ENTER para sair')
