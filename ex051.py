33f283201b494461c8487b135ef9a736bd75185c
Od Zarref <ludwigvonmarcelo@gmail.com>
2019-08-11 18:53:39 -0300

    Resolving ex051.py

--- ex051.py
+++ ex051.py
@@ -1,1 +1,8 @@
 #Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
+termo = int(input('PRIMEIRO TERMO: '))
+razao = int(input('RAZÃO: '))
+for c in range(1, 11):
+	print(f'{c:2}º = {termo}')
+	termo += razao
+print('-=' * 13)
+input('pressione ENTER para sair')
