9780af067446e8a91b236108f82f69785733a0c8
Od Zarref <ludwigvonmarcelo@gmail.com>
2019-08-11 18:32:50 -0300

    Resolving ex050.py

--- ex050.py
+++ ex050.py
@@ -1,1 +1,9 @@
 #Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
+s = 0
+for c in range(1, 7):
+	num = int(input(f'{c}º NÚMERO: '))
+	if num % 2 == 0:
+		s += num
+print(f'A soma dos números pares digitados é {s}')
+print('-=' * 20)
+input('pressione ENTER para sair')
