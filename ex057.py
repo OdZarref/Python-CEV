500ca93c736bd0cc3dce53c9a327fccab0c26045
Od Zarref <ludwigvonmarcelo@gmail.com>
2019-08-13 19:23:44 -0300

    Adding exercises

---
+++ ex057.py
@@ -0,0 +1,1 @@
+#Faça um programa que leio o sexo de uma pessoa, mas só aceite os valores 'M' ou 'F'. Caso esteja errado, peça a digitação novamente até ter um valor correto.
---
+++ ex058.py
@@ -0,0 +1,1 @@
+#Melhore o jogo do DESAFIO 028 onde o computador vai "pensar" em uma número entre 0 e 10. Só que agora o jogador vai tentar adivinhar até ele acertar, mostrando no final quantos palpites foram necessários para vencer.
