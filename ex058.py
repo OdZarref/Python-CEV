d8105fe809609391e5d7c3d36028e3389873d963
Od Zarref <ludwigvonmarcelo@gmail.com>
2019-08-17 12:49:38 -0300

    Resolving ex058.py

--- ex058.py
+++ ex058.py
@@ -1,1 +1,17 @@
 #Melhore o jogo do DESAFIO 028 onde o computador vai "pensar" em uma número entre 0 e 10. Só que agora o jogador vai tentar adivinhar até ele acertar, mostrando no final quantos palpites foram necessários para vencer.
+from random import randint
+print('TENTE ACERTAR O NÚMERO QUE O COMPUTADOR PENSAR!')
+print('-=' * 20)
+
+jogador = int(input('SEU PALPITE? '))
+maquina = randint(0, 11)
+quantidade_de_tentativas = 0
+
+while jogador != maquina:
+	print('-=' * 20)
+	quantidade_de_tentativas += 1
+	jogador = int(input('ERROU! QUAL SEU NOVO PALPITE? '))
+
+print('-=' * 20)
+print(f'VOCÊ ACERTOU! E FORAM NECESSÁRIOS {quantidade_de_tentativas} TENTATIVAS PARA VOCÊ ACERTAR!')
+input('pressione ENTER para sair')
