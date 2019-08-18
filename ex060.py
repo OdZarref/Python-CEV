fb3ef84d7eaa7fda54ce38e550575384c77c80b7
Od Zarref <ludwigvonmarcelo@gmail.com>
2019-08-18 14:41:00 -0300

    Resolving ex060.py

--- ex060.py
+++ ex060.py
@@ -1,1 +1,15 @@
 #Faça um programa que leia um número qualquer e mostre o seu fatorial. Ex: 5!=5x4x3x2x1 = 120
+numero = int(input('NÚMERO: '))
+contador = numero
+resultado = numero
+print('-=' * 13)
+print(numero, end='')
+
+while contador != 1:
+	contador -= 1
+	resultado = resultado * contador
+	print(f'x{contador}', end='')
+
+print(f'={resultado}')
+print('-=' * 13)
+input('pressione ENTER para sair')
