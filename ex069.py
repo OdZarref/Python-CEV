3759630cfa5b067ad18b64018a1af547a6a5ed26
Od Zarref <ludwigvonmarcelo@gmail.com>
2020-01-14 23:00:45 -0300

    Adicionando exercícios resolvidos

--- ex069.py
+++ ex069.py
@@ -1,38 +1,35 @@
-#Crie um programa que leia a idade e o sexo de várias pessoas. A cada pessoa cadastrada, o programa deverá perguntar se o usuário quer ou não continuar. No final, mostre: A) Quantas pessoas tem mais de 18 anos. B) Quantos homens foram cadastrados. C) Quantas mulheres tem menos de 20 anos.
-print('-=' * 20)
-print('DIGITE OS DADOS PARA CADASTRO')
-print('-=' * 20)
-mais18 = mulhermenos20 = homens = 0
+#Crie um programa que leia a idade e o sexo de várias pessoas. A cada pessoa cadastranda, o programa deverá perguntar se o usuário quer ou não continuarç. No final, mostre: A) Quantas pessoas tem mais de 18 anos. B) Quantos homens foram cadastrados. C) Quantas mulheres tem menos de 20 anos.
+quantos_mais_dezoito = quantos_homens = mulheres_menos_vinte = 0
 
 while True:
-	idade = int(input('IDADE: '))
+	 continuar = sexo = ''
+	 idade = int(input('Idade: '))
 
-	while True:
-		sexo = str(input('SEXO: [ M / F ]')).strip().upper()[0]
-		print('-=' * 20)
+	 while 'M' not in sexo and 'F' not in sexo:
+		 sexo = str(input('Sexo: [M/F]')).upper()[0]
 
-		if sexo != 'F' and sexo != 'M':
-			print('SEXO INVÁLIDO! TENTE NOVAMENTE.')
-			print('-=' * 20)
-		else:
-			break
+	 if idade > 18:
+	 	quantos_mais_dezoito += 1
 
-	continuar = str(input('QUER CONTINUAR? [ S / N ]')).strip().upper()[0]
-	
-	if sexo == 'F' and idade < 20:
-		mulhermenos20 += 1
+	 if 'M' in sexo:
+	 	quantos_homens += 1
 
-	if sexo == 'M':
-		homens += 1
+	 if 'F' in sexo and idade < 20:
+	 	mulheres_menos_vinte += 1
 
-	if idade > 18:
-		mais18 += 1
+	 while 'S' not in continuar and 'N' not in continuar:
+		 continuar = str(input('Continuar? [S/N] ')).upper()[0]
 
-	if continuar != 'S':
-		break
+	 if 'N' in continuar:
+	 	break
 
-print(f'A QUANTIDADE DE PESSOAS QUE TEM MAIS DE 18 É {mais18}.')
-print(f'A QUANTIDADE DE HOMENS CADASTROS É {homens}.')
-print(f'A QUANTIDADE DE MULHERES COM MENOS DE 20 É {mulhermenos20}.')
-print('-=' * 20)
-input('pressione ENTER para sair')
+	 print('=' * 16)
+
+print('=' * (33 + len(str(mulheres_menos_vinte))))
+print(f'{"PROGRAMA FINALIZADO":^33}')
+print('=' * (33 + len(str(mulheres_menos_vinte))))
+print(f'Total de pessoas maiores de 18: {quantos_mais_dezoito}')
+print(f'Total de homens cadastrados: {quantos_homens}')
+print(f'Total de mulheres menores de 20: {mulheres_menos_vinte}')
+print('=' * (33 + len(str(mulheres_menos_vinte))))
+input('pressione qualquer tecla para sair')
